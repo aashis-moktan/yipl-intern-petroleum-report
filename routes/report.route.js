08ca2914933ce04be8e1ced265ba2e7a9a357123
Aashish Moktan <aashiislama@gmail.com>
2022-03-05 11:57:38 +0545

    Added routing and code for getting highest sales and lowest sales by country

--- routes/report.route.js
+++ routes/report.route.js
@@ -25,4 +25,28 @@ router.get('/total-sale-by-petroleum-product',(request, response)=>{
     db.close();
 });
 
+// GET HIGHEST AND LOWEST SALES
+router.get('/highest-lowest-total-sales',(request, response)=>{
+    const db = new sqlite3.Database(DBSOURCE, (error)=>{
+        if(error)
+            response.status(500).json({message:'Failed to connect with database'});
+        else{
+            const data = {
+                "highest_sales_countries": "",
+                "lowest_sales_countries": ""
+            }
+            db.all('SELECT country, SUM(sale) as total_sales FROM Petroleum GROUP BY Country ORDER BY SUM(sale) DESC',[],(error, rows)=>{
+                if(error)
+                    response.status(500).json({message: 'Internal Server Error'});
+                else
+                    data["highest_sales_countries"] = rows.slice(0,3);
+                    data["lowest_sales_countries"] = rows.slice(rows.length-3).reverse();
+                    response.status(200).json(data);
+
+            });
+        }
+        db.close();
+    });
+});
+
 module.exports = router;
