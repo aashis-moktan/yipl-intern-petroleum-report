f41eadba02bf60d782d33fc622fb3521724a779d
Aashish Moktan <aashiislama@gmail.com>
2022-03-05 12:34:49 +0545

    Routing added for getting average sale between 4 years interval

--- routes/report.route.js
+++ routes/report.route.js
@@ -25,7 +25,7 @@ router.get('/total-sale-by-petroleum-product',(request, response)=>{
     db.close();
 });
 
-// GET HIGHEST AND LOWEST SALES
+// GET HIGHEST AND LOWEST SALES BY SALES
 router.get('/highest-lowest-total-sales',(request, response)=>{
     const db = new sqlite3.Database(DBSOURCE, (error)=>{
         if(error)
@@ -46,6 +46,38 @@ router.get('/highest-lowest-total-sales',(request, response)=>{
             });
         }
         db.close();
+    });
+});
+
+// GET AVERAGE SALE OF EACH PETROLEUM PRODUCT FOR 4 YEARS OF INTERVAL
+router.get('/average-sale',(request, response)=>{
+    const db = new sqlite3.Database(DBSOURCE, (error)=>{
+        if(error)
+            response.status(500).json({message:'Failed to connect with database'});
+        else {
+            db.all(`SELECT petroleum_product,
+                CASE year 
+                    WHEN 2007 then '2007-2010' 
+                    WHEN 2008 then '2007-2010'
+                    WHEN 2009 then '2007-2010'
+                    WHEN 2010 then '2007-2010'
+                    WHEN 2011 then '2011-2014'
+                    WHEN 2012 then '2011-2014' 
+                    WHEN 2013 then '2011-2014'
+                    WHEN 2014 then '2011-2014'
+                    ELSE year 
+                END year,
+                AVG(total) as Avg 
+                FROM (SELECT petroleum_product, 
+                    SUM(sale) as total, year FROM Petroleum GROUP BY petroleum_product, year HAVING sale != 0) GROUP BY petroleum_product, year BETWEEN 2007 AND 2010, year BETWEEN 2011 AND 2014 ORDER BY petroleum_product, year`,[],(error,rows)=>{
+                if(error){
+                    response.status(500).json({message: 'Internal Server Error'});
+                }
+                else {
+                    response.status(200).json(rows);
+                }
+            });
+        }
     });
 });
 
